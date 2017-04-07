bf50371a3512d9ac69a7ff00d1889bc4a237d8c5
Megan Coyle <mcoyle@usgbc.org>
2017-04-07 17:17:03 -0400

    Add comments and online config

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 const app = express();
+// log requests to console
 app.use(logger('dev'));
+// parse incoming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./server/routes')(app);
+// catch-all route
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
 }));
